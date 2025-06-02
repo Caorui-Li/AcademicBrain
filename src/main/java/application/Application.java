afec325966e09db874a435754eaeee214953e539
Caorui Li <2382461164@qq.com>
2025-06-02 21:17:33 +0800

    modify: 添加运行测试和数据库测试（save a user ）

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,9 +1,30 @@
+package application;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import model.User;
+import org.springframework.context.annotation.Bean;
+import repository.UserRepository;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"application", "repository", "model"})
+@EnableJpaRepositories(basePackages = "repository")
+@EntityScan(basePackages = "model")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(UserRepository userRepository) {
+        return args -> {
+            User user = new User();
+            user.setUserId("u001");
+            user.setNickname("Alice");
+
+            userRepository.save(user);
+        };
     }
 }
